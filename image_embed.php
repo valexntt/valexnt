03e389154ed2e353f138b5ebcef9c136810c2b92
valexnt <127977224+valexntt@users.noreply.github.com>
2023-11-06 01:27:53 +0100

    Update image_embed.php

--- image_embed.php
+++ image_embed.php
@@ -1,27 +1,26 @@
 <?php
-$directory = 'img'; // No need for a full path; it's in the same directory as index.html
+$directory = 'img'; // Directory where your images are stored
 
-// Get the list of .png files in the folder
-$pngFiles = glob($directory . '/*.png');
+// Get the list of files in the folder
+$files = scandir($directory);
+
+// Filter .png files
+$pngFiles = array_filter($files, function($file) {
+    return pathinfo($file, PATHINFO_EXTENSION) == 'png';
+});
 
 if (!empty($pngFiles)) {
     // Use the first .png file as the OG image
-    $ogImage = 'https://yourwebsite.com/' . $directory . '/' . basename($pngFiles[0]);
-    
-    // Set the image dimensions (adjust these values as needed)
-    $width = 400;
-    $height = 300;
+    $imageURL = 'https://yourwebsite.com/' . $directory . '/' . reset($pngFiles);
 
-    // Set the dynamic OG tags with centered image
-    echo '<meta property="og:image" content="' . $ogImage . '" />' . PHP_EOL;
+    // Set the dynamic OG tags
+    echo '<meta property="og:image" content="' . $imageURL . '" />' . PHP_EOL;
     echo '<meta property="og:image:type" content="image/png" />' . PHP_EOL;
-    echo '<meta property="og:image:width" content="' . $width . '" />' . PHP_EOL;
-    echo '<meta property="og:image:height" content="' . $height . '" />' . PHP_EOL;
-    echo '<meta property="og:image:height" content="image/png" />' . PHP_EOL;
-    echo '<meta property="og:image:height" content="center" />' . PHP_EOL;
+    echo '<meta property="og:image:width" content="400" />' . PHP_EOL;
+    echo '<meta property="og:image:height" content="300" />' . PHP_EOL;
     echo '<meta property="og:description" content="Your image description" />' . PHP_EOL;
 } else {
-    // If there are no .png files, use a default image
-    echo '<meta property="og:image" content="https://yourwebsite.com/img_placeholder.png" />' . PHP_EOL;
+    // If there are no .png files, do not generate OG tags
+    echo '<!-- No .png images found -->';
 }
 ?>
