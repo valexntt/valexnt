5496d8c9f01c5d9e65cdb786ad7eeedcdc7869b0
valexnt <127977224+valexntt@users.noreply.github.com>
2023-11-06 01:23:31 +0100

    Update image_embed.php

--- image_embed.php
+++ image_embed.php
@@ -1,21 +1,24 @@
 <?php
-$directory = 'img'; // Update the path to your /img folder
+$directory = 'img'; // No need for a full path; it's in the same directory as index.html
 
 // Get the list of .png files in the folder
-$pngFiles = glob($directory . '/*.PNG');
+$pngFiles = glob($directory . '/*.png');
 
 if (!empty($pngFiles)) {
     // Use the first .png file as the OG image
-    $ogImage = 'https://valexnt.lol/img/' . basename($pngFiles[0]);
+    $ogImage = 'https://yourwebsite.com/' . $directory . '/' . basename($pngFiles[0]);
     
-    // Get the image dimensions
-    list($width, $height) = getimagesize($pngFiles[0]);
+    // Set the image dimensions (adjust these values as needed)
+    $width = 400;
+    $height = 300;
 
-    // Set the dynamic OG tags
+    // Set the dynamic OG tags with centered image
     echo '<meta property="og:image" content="' . $ogImage . '" />' . PHP_EOL;
     echo '<meta property="og:image:type" content="image/png" />' . PHP_EOL;
     echo '<meta property="og:image:width" content="' . $width . '" />' . PHP_EOL;
     echo '<meta property="og:image:height" content="' . $height . '" />' . PHP_EOL;
+    echo '<meta property="og:image:height" content="image/png" />' . PHP_EOL;
+    echo '<meta property="og:image:height" content="center" />' . PHP_EOL;
     echo '<meta property="og:description" content="Your image description" />' . PHP_EOL;
 } else {
     // If there are no .png files, use a default image
